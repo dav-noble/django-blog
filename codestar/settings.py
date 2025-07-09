1d4a4e6ea3726bffa89ac4ac7d512221c4e910f0
dav-noble <davidnoble26@gmail.com>
2025-07-09 10:41:30 +0100

    Update: ALLOWED HOSTS

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_qs_^jr8u9x_ujtqaygv2vq&4fyu!!g&jfonve_g)vid5y*xn2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+]
 
 
 # Application definition
